c8c53f318cfd7a3e38a16d19e970bca4159cf1e8
Hebert Soares <hebertsoaresof@gmail.com>
2024-12-04 14:47:18 -0300

    Corrindo erros do build

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Collection from './pages/Collection';
 import About from './pages/About';
-import Contact from './pages/Contact';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
@@ -30,7 +29,6 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
         <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
         <Route path='/product/:productId' element={<Product />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/place-order' element={<PlaceOrder />} />
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,29 +1,20 @@
 
 import logo from './logo.ico'
 import hero_img from './hero_img.png'
-import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
-import dropdown_icon from './dropdown_icon.png'
-import exchange_icon from './exchange_icon.png'
-import profile_icon from './profile_icon.png'
 import quality_icon from './quality_icon.png'
 import search_icon from './search_icon.png'
 import star_dull_icon from './star_dull_icon.png'
 import star_icon from './star_icon.png'
 import support_img from './support_img.png'
-import menu_icon from './menu_icon.png'
 import about_img from './about_img.png'
 import contact_img from './contact_img.png'
-import razorpay_logo from './razorpay_logo.png'
-import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
 
 export const assets = {
     logo,
     hero_img,
     cart_icon: 'https://i.imgur.com/xt08AG3.png',
-    dropdown_icon,
-    exchange_icon,
     profile_icon: 'https://i.imgur.com/p6NpdMj.png',
     quality_icon,
     search_icon,
@@ -31,10 +22,7 @@ export const assets = {
     star_icon,
     bin_icon,
     support_img,
-    menu_icon,
     about_img,
     contact_img,
-    razorpay_logo,
-    stripe_logo,
     cross_icon
 }
